83bdd5bc8dbf320410ac212eee36e31019aec0f3
tomrigolo <tomjegou10@gmail.com>
2022-10-03 13:48:11 +0200

    improving implementaion

--- src/lib/engine.go
+++ src/lib/engine.go
@@ -13,28 +13,14 @@ func Engine(words []string) {
 	hiddenWord := ""
 	numberLetterRevealed := len(word_to_guess)/2 - 1
 	for i := 0; i < len(word_to_guess); i++ {
-		if i == len(word_to_guess)-1 {
-			hiddenWord += "_"
-		} else {
-			hiddenWord += "_"
-		}
+		hiddenWord += "_"
 	}
 	slice_byte_hidden := []byte(hiddenWord)
 	for i := 0; i < numberLetterRevealed; i++ {
 		indexLetterRevealed := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(slice_byte_hidden))
 		slice_byte_hidden[indexLetterRevealed] = word_to_guess[indexLetterRevealed]
 	}
-	finalHiddenWord := ""
-	for i := 0; i < len(slice_byte_hidden); i++ {
-		if i == len(slice_byte_hidden)-1 {
-			finalHiddenWord += string(slice_byte_hidden[i])
-		} else {
-			finalHiddenWord += string(slice_byte_hidden[i]) + " "
-		}
-	}
 	fmt.Println(word_to_guess)
-	fmt.Println(letterRemaining)
-	currentStateWord := finalHiddenWord
-	DisplayInput(currentStateWord, attemptRemaining)
-
+	fmt.Println(RemainingLetter(slice_byte_hidden, word_to_guess))
+	DisplayInput(slice_byte_hidden, attemptRemaining)
 }
