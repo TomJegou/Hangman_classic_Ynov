09b959bd54eece4c96153dd4cd7b09edf43f5995
L0uxe <loulourxl@hotmail.com>
2022-10-03 11:27:49 +0200

    WIP displayHangman

---
+++ src/lib/displayHang.go
@@ -0,0 +1,24 @@
+package lib
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func DisplayHangman(attempt int) {
+	file, err := os.Open("../Templates/hangman.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	draw := []string{}
+	scanner := bufio.NewScanner(file)
+	for i := 0; i <= 8; i++ {
+		for scanner.Scan() {
+			draw = append(draw, scanner.Text())
+			break
+		}
+
+	}
+	fmt.Print(draw)
+}
