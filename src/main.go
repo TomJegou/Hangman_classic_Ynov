ad083327cfd71d9316e58bf7f385fd3b8308810f
tomrigolo <tomjegou10@gmail.com>
2022-10-03 14:18:39 +0200

    adding error if no args given

--- src/main.go
+++ src/main.go
@@ -8,18 +8,22 @@ import (
 )
 
 func main() {
-	dic := os.Args[1:]
-	file, err := os.Open("../dictionnaries/" + dic[0] + ".txt")
-	if err != nil {
-		lib.Clear()
-		fmt.Println(err)
-	} else {
-		all_word := []string{}
-		scanner := bufio.NewScanner(file)
-		scanner.Split(bufio.ScanLines)
-		for scanner.Scan() {
-			all_word = append(all_word, scanner.Text())
+	if len(os.Args) > 1 {
+		dic := os.Args[1:]
+		file, err := os.Open("../dictionnaries/" + dic[0] + ".txt")
+		if err != nil {
+			lib.Clear()
+			fmt.Println(err)
+		} else {
+			all_word := []string{}
+			scanner := bufio.NewScanner(file)
+			scanner.Split(bufio.ScanLines)
+			for scanner.Scan() {
+				all_word = append(all_word, scanner.Text())
+			}
+			lib.Engine(all_word)
 		}
-		lib.Engine(all_word)
+	} else {
+		fmt.Println("No dictionnary given")
 	}
 }
