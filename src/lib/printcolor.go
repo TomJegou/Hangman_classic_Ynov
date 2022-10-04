d12f2b6e2ee417a49c995d0c3c8ed1bc86eb3c99
tomrigolo <tomjegou10@gmail.com>
2022-10-04 12:34:36 +0200

    Adding function printcolor

---
+++ src/lib/printcolor.go
@@ -0,0 +1,16 @@
+package lib
+
+import "fmt"
+
+func PrintColor(s, color string) {
+	color_map := map[string]string{
+		"Black":  "\033[0;30m",
+		"Red":    "\033[0;31m",
+		"Green":  "\033[0;32m",
+		"Yellow": "\033[0;33m",
+		"Blue":   "\033[0;34m",
+		"Purple": "\033[0;35m",
+		"Cyan":   "\033[0;36m",
+		"White":  "\033[0;37m"}
+	fmt.Printf("%v%v\n", color_map[color], s)
+}
