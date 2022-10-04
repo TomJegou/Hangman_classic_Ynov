802419f3e8edac19a61c3b1988430331c282cb91
tomrigolo <tomjegou10@gmail.com>
2022-10-04 09:05:48 +0200

    Finished function checkTwice

--- src/lib/checkTwice.go
+++ src/lib/checkTwice.go
@@ -1,5 +1,12 @@
 package lib
 
-func Checktwice(letter string, sliceChar []string) {
+import "fmt"
 
+func Checktwice(letter string, t []string) []string {
+	if IsIn(t, letter) {
+		fmt.Println("Already done")
+	} else {
+		t = append(t, letter)
+	}
+	return t
 }
