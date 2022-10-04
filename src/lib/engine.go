b4bc86cd0f8a0f261c7394cd49eff837b227a803
tomrigolo <tomjegou10@gmail.com>
2022-10-04 10:37:50 +0200

    Implementing menu to quit and start new game

--- src/lib/engine.go
+++ src/lib/engine.go
@@ -2,108 +2,16 @@ package lib
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
-func Engine(words []string) {
-	Clear()
-	debug_mod := false
-	maxError := 10
-	numberError := 0
-	word_to_guess := ChoseRandomWord(words)
-	hiddenWord := ""
-	sliceAllChar := []string{}
-	inputHistory := []string{}
-	for i := 0; i < len(word_to_guess); i++ {
-		sliceAllChar = append(sliceAllChar, string(word_to_guess[i]))
-	}
-	numberLetterRevealed := len(word_to_guess)/2 - 1
-	for i := 0; i < len(word_to_guess); i++ {
-		hiddenWord += "_"
-	}
-	slice_byte_hidden := []byte(hiddenWord)
-	for i := 0; i < numberLetterRevealed; i++ {
-		indexLetterRevealed := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(slice_byte_hidden))
-		DiscoverLetter(slice_byte_hidden, string(word_to_guess[indexLetterRevealed]), word_to_guess)
-		inputHistory, _ = Checktwice(string(word_to_guess[indexLetterRevealed]), inputHistory)
-	}
-	remainLetter := RemainingLetter(slice_byte_hidden, word_to_guess)
-	found := true
+func Engine(lists_words []string) {
 	var input string
-	attempt_number := 0
-	invalid_ouput := false
-	twice := false
-	for len(remainLetter) > 0 {
-		attempt_number++
-		Clear()
-		if debug_mod {
-			fmt.Println("Word to find: " + word_to_guess)
-			fmt.Printf("Number error max: %v\n", maxError)
-			fmt.Printf("Number error: %v\n", numberError)
-			fmt.Printf("Attempt number: %v\n", attempt_number)
-			if len(inputHistory) > 0 {
-				fmt.Print("Input history")
-				fmt.Println(inputHistory)
-			}
-			if len(input) > 0 {
-				fmt.Println("Last input player: " + input)
-			}
-			fmt.Println()
+	for input != "q" {
+		fmt.Println("[q] quit\n[s] start new game")
+		fmt.Printf("Choose: ")
+		fmt.Scan(&input)
+		if input == "s" {
+			Game(lists_words)
 		}
-		if numberError >= maxError {
-			found = false
-			break
-		}
-		if numberError > 0 {
-			if invalid_ouput {
-				fmt.Println("Invalid input, only one alphabetical character is supported in entry")
-				invalid_ouput = false
-			} else if twice {
-				fmt.Println("Already try this letter")
-				twice = false
-			} else {
-				DisplayWrongLetter(numberError, maxError)
-			}
-			DisplayHangman(numberError)
-		}
-		DisplayInput(slice_byte_hidden, numberError)
-		fmt.Scanln(&input)
-		if len(input) < 1 {
-			numberError++
-			invalid_ouput = true
-			continue
-		}
-		if len(input) > 1 {
-			if input == word_to_guess {
-				break
-			} else {
-				numberError += 2
-				continue
-			}
-		}
-		if input >= "0" && input <= "9" {
-			numberError++
-			invalid_ouput = true
-			continue
-		}
-		inputHistory, twice = Checktwice(input, inputHistory)
-		if IsIn(sliceAllChar, input) && IsIn(remainLetter, input) {
-			DiscoverLetter(slice_byte_hidden, input, word_to_guess)
-			remainLetter = RemainingLetter(slice_byte_hidden, word_to_guess)
-		} else {
-			numberError++
-		}
-	}
-	if found {
-		Clear()
-		DisplayHangman(numberError)
-		fmt.Println("\033[32m", "Congrat !")
-		fmt.Println("\033[32m", "You've found the word !")
-		fmt.Println("\033[32m", "The word was: "+word_to_guess)
-	} else {
-		DisplayHangman(numberError)
-		fmt.Println("\033[31m", "You didn't find the word !")
-		fmt.Println("\033[31m", "The word was: "+word_to_guess)
 	}
 }
