0e9c4c3a268644eb1aa0364a2a9d8bbf4da7e6ea
L0uxe <loulourxl@hotmail.com>
2022-10-04 09:07:49 +0200

    modif err mess

--- src/lib/checkTwice.go
+++ src/lib/checkTwice.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func Checktwice(letter string, t []string) []string {
 	if IsIn(t, letter) {
-		fmt.Println("Already done")
+		fmt.Println("You already try this letter ")
 	} else {
 		t = append(t, letter)
 	}
