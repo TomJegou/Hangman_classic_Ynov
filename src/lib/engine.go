56d30b39b5de117e2d524351c42fb967ab4c853f
tomrigolo <tomjegou10@gmail.com>
2022-10-04 10:49:29 +0200

    Implementing invalid output

--- src/lib/engine.go
+++ src/lib/engine.go
@@ -5,13 +5,29 @@ import (
 )
 
 func Engine(lists_words []string) {
+	Clear()
 	var input string
-	for input != "q" {
+	keep_playing := true
+	invalid_ouput := false
+	for keep_playing {
+		if invalid_ouput {
+			Clear()
+			fmt.Print("Invalid output !\n\n")
+			invalid_ouput = false
+		} else {
+			fmt.Print("Welcome to Classic_Hangman made by jtom and rlouis !\n\n")
+		}
 		fmt.Println("[q] quit\n[s] start new game")
 		fmt.Printf("Choose: ")
 		fmt.Scan(&input)
 		if input == "s" {
 			Game(lists_words)
+		} else if input == "q" {
+			Clear()
+			fmt.Println("Thanks for Playing !")
+			keep_playing = false
+		} else {
+			invalid_ouput = true
 		}
 	}
 }
