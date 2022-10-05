96684adb5ffea494dc680dd1705e0fb6e1483daf
L0uxe <loulourxl@hotmail.com>
2022-10-05 10:52:13 +0200

    Wip

--- src/lib/displayasciiletter.go
+++ src/lib/displayasciiletter.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func DisplayAsciiWord(word string) { //test
+func DisplayAsciiWord(word string) {
 	for i := 0; i < len(word); i++ {
 		w := []byte(string(word[i]))
 		DisplayAsciiLetter(w)
