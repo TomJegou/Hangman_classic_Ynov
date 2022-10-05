75e617e63a489d617387377683ef6b0e9bcd0ed9
L0uxe <loulourxl@hotmail.com>
2022-10-05 17:24:51 +0200

    add comment

--- src/lib/checkTwice.go
+++ src/lib/checkTwice.go
@@ -1,9 +1,12 @@
 package lib
 
+/*
+Here we check if the letter as been already play if yes it return true otherwhise it's return false
+*/
 func Checktwice(letter string, t []string) ([]string, bool) {
-	if IsIn(t, letter) {
+	if IsIn(t, letter) { // check if the letter is in
 		return t, true
 	}
-	t = append(t, letter)
+	t = append(t, letter) // append the letter that who just been played and are not already in the tab
 	return t, false
 }
