65e381cd27249eb4af418d48b77b60388c0d4383
tomrigolo <tomjegou10@gmail.com>
2022-10-08 17:13:19 +0200

    comments

--- src/lib/checkTwice.go
+++ src/lib/checkTwice.go
@@ -1,7 +1,7 @@
 package lib
 
 /*
-Here we check if the letter parameter is twice in a string table t,
+Check if the letter parameter is twice in a string table t,
 if yes it returns true and the table otherwhise it append the letter in t and returns false
 */
 func Checktwice(letter string, t []string) ([]string, bool) {
