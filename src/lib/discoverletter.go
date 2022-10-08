9685efe9f84cd13d301aea49272f61c9510cffa0
tomrigolo <tomjegou10@gmail.com>
2022-10-08 17:34:25 +0200

    adding comments

--- src/lib/discoverletter.go
+++ src/lib/discoverletter.go
@@ -1,5 +1,9 @@
 package lib
 
+/*
+Fuction used to change the "_" to the corresponding letter from the word
+*/
+
 func DiscoverLetter(t []byte, letter string, word string) {
 	for i := 0; i < len(word); i++ {
 		if string(word[i]) == letter {
