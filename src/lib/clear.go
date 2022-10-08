b990fc5517967b6b90a7642df73e8e2ab61a1427
tomrigolo <tomjegou10@gmail.com>
2022-10-08 17:16:17 +0200

    correcting comments

--- src/lib/clear.go
+++ src/lib/clear.go
@@ -3,9 +3,9 @@ package lib
 import "fmt"
 
 /*
-The function clear is use to clear the consol to play
+Function that clears the terminal output
 For that we used the ANSI Escape Sequences
 */
 func Clear() {
-	fmt.Print("\033[H\033[2J") // the \033[h is used to moove the cursor to the 0 0 positon and \033[j to clear the consol
+	fmt.Print("\033[H\033[2J") // the \033[h is used to moove the cursor to the 0 0 positon and \033[j to clear the terminal
 }
