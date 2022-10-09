0ebf5150ded24777d2e4e499bb3a1b71454ac528
tomrigolo <tomjegou10@gmail.com>
2022-10-09 22:47:24 +0200

    correcting error in the comment

--- src/lib/clear.go
+++ src/lib/clear.go
@@ -7,5 +7,5 @@ Function that clears the terminal output
 We use the ANSI Escape Sequences
 */
 func Clear() {
-	fmt.Print("\033[H\033[2J") // the \033[h is used to moove the cursor to the 0 0 positon and \033[j to clear the terminal
+	fmt.Print("\033[H\033[2J") // the \033[h is used to move the cursor to the 0 0 positon and \033[j to clear the terminal
 }
