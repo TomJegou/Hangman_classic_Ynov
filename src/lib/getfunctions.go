00bbbec76e29da388e9aaeab6c41e16485d99e67
TomJegou <tomjegou10@gmail.com>
2022-10-09 20:00:45 +0200

    removing getline function, useless

--- src/lib/getfunctions.go
+++ src/lib/getfunctions.go
@@ -15,6 +15,7 @@ func GetFileInLine(file_path string) []string {
 	if err != nil {
 		Clear()
 		PrintColor("This file doesn't exist\n", "Red")
+		os.Exit(0)
 	} else {
 		scanner := bufio.NewScanner(file)
 		scanner.Split(bufio.ScanLines)
@@ -27,8 +28,4 @@ func GetFileInLine(file_path string) []string {
 
 func Getpositioninalphabet(letter byte) int {
 	return int(letter) - 65
-}
-
-func Getline(line_number int, template_name string) string {
-	return GetFileInLine("../Templates/" + template_name + ".txt")[line_number]
 }
