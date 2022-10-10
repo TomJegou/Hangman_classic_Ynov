a266c690ce4af61483ec5f48674ef5c61e9c4ca7
L0uxe <loulourxl@hotmail.com>
2022-10-10 11:11:34 +0200

    implentation scandir

---
+++ src/lib/scandir.go
@@ -0,0 +1,17 @@
+package lib
+
+import (
+	"os"
+)
+
+func Scandir(pathdir string) map[string]string {
+	data := map[string]string{}
+	files, err := os.ReadDir(pathdir)
+	if err != nil {
+		PrintColor("Dictionnary Error", "Red")
+	}
+	for i := 0; i < len(files); i++ {
+		data[string(byte(i+48))] = files[i].Name()[:len(files[i].Name())-4]
+	}
+	return data
+}
