85a242a22648cf586da5f200c9563c8c4664cade
L0uxe <loulourxl@hotmail.com>
2022-10-10 11:59:14 +0200

    add templates_names in game

--- src/lib/game.go
+++ src/lib/game.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func Game(lists_words []string, display_mod string) {
+func Game(lists_words []string, display_mod string, templates_names map[string]string) {
 	Clear()
 	debug_mod := false // Boolean to activate the debug mod
 	maxError := 10
@@ -75,7 +75,7 @@ func Game(lists_words []string, display_mod string) {
 			}
 			DisplayHangman(numberError)
 		}
-		DisplayModLetter(slice_byte_hidden, display_mod)
+		DisplayModLetter(slice_byte_hidden, display_mod, templates_names)
 		fmt.Scanln(&input) // get the intput player
 		// check the input validity
 		if len(input) < 1 {
@@ -109,11 +109,11 @@ func Game(lists_words []string, display_mod string) {
 	if found {
 		Clear()
 		DisplayHangman(numberError)
-		DisplayModLetter(slice_byte_hidden, display_mod)
+		DisplayModLetter(slice_byte_hidden, display_mod, templates_names)
 		PrintColor("\nCongrat !\nYou've found the word\nThe word was: "+word_to_guess+"\n\n", "Green")
 	} else {
 		DisplayHangman(numberError)
-		DisplayModLetter(slice_byte_hidden, display_mod)
+		DisplayModLetter(slice_byte_hidden, display_mod, templates_names)
 		PrintColor("\nYou didn't find the word !\nThe word was: "+word_to_guess+"\n\n", "Red")
 	}
 	//loop to ask the player to keep playing or not
@@ -130,7 +130,7 @@ func Game(lists_words []string, display_mod string) {
 			Clear()
 			PrintColor("Starting new game...", "White")
 			time.Sleep(1 * time.Second)
-			Game(lists_words, display_mod)
+			Game(lists_words, display_mod, templates_names)
 		} else if input == "q" {
 			Clear()
 			PrintColor("Thanks for playing !", "White")
