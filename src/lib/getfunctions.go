72ac1f6ea6cf237aa9996d8b70888466ff653c2b
L0uxe <loulourxl@hotmail.com>
2022-10-10 09:57:40 +0200

    add comment to getfunction

--- src/lib/getfunctions.go
+++ src/lib/getfunctions.go
@@ -9,15 +9,19 @@ import (
 File that contains all the get functions
 */
 
+/*
+Function that open a file and return a slice of all lines
+*/
+
 func GetFileInLine(file_path string) []string {
-	data := []string{}
+	data := []string{} // slice wich sotre all lines
 	file, err := os.Open(file_path)
 	if err != nil {
 		Clear()
 		PrintColor("This file doesn't exist\n", "Red")
 		os.Exit(0)
 	} else {
-		defer file.Close()
+		defer file.Close() // close the file at the end of the function
 		scanner := bufio.NewScanner(file)
 		scanner.Split(bufio.ScanLines)
 		for scanner.Scan() {
@@ -26,6 +30,10 @@ func GetFileInLine(file_path string) []string {
 	}
 	return data
 }
+
+/*
+Function that return the postition of the letter in the alphabet
+*/
 
 func Getpositioninalphabet(letter byte) int {
 	return int(letter) - 65
