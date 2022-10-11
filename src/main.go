62e7bb0d3debbe64f6586a5253334f0934ba22f8
L0uxe <loulourxl@hotmail.com>
2022-10-11 09:22:12 +0200

    change main.go

--- src/main.go
+++ src/main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"hangman_classic/lib"
-	"os"
 )
 
 func main() {
-	if len(os.Args) > 1 {
-		dic := os.Args[1:]
-		lib.Engine(lib.GetFileInLine("../dictionnaries/" + dic[0] + ".txt"))
-	} else {
-		lib.PrintColor("No dictionnary given\n", "Red")
-	}
+	lib.Engine()
 }
