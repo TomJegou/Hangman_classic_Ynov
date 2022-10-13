16b8cb401fe950b60c768cc87c732fe895a78bf9
L0uxe <loulourxl@hotmail.com>
2022-10-13 11:51:07 +0200

    refactoring

--- src/lib/displayfunctions.go
+++ src/lib/displayfunctions.go
@@ -1,8 +1,29 @@
 package lib
 
 import (
+	"fmt"
 	"strconv"
+	"time"
 )
+
+/*
+Function used to print texts in color
+We use the ANSI Escape Sequences
+*/
+
+func PrintColor(s, color string) {
+	color_map := map[string]string{ //map to store the corresponding color's ansi code
+		"Black":  "\033[0;30m",
+		"Red":    "\033[0;31m",
+		"Green":  "\033[0;32m",
+		"Yellow": "\033[0;33m",
+		"Blue":   "\033[0;34m",
+		"Purple": "\033[0;35m",
+		"Cyan":   "\033[0;36m",
+		"White":  "\033[0;37m"}
+	fmt.Printf("%v%v", color_map[color], s)
+	fmt.Printf("%v", color_map["White"])
+}
 
 /*
 Function that displays the current state of the hangman according to the number of errors
@@ -53,4 +74,33 @@ func DisplayModLetter(save *Save, color string, withSpace, input bool) {
 	if input {
 		PrintColor("Choose: ", "White") //Input display
 	}
+}
+
+/*
+Func that makes a loding bar
+
+[░░░░░░░░░░░░░░░░░░░░░░░░░░░]
+[███████████░░░░░░░░░░░░░░░░]
+[███████████████████████████]
+
+░ 176
+█ 219
+*/
+
+func LoadingBar(t time.Duration) { //t = Duration in millisecond
+	ldbar := "[░░░░░░░░░░░░░░░░░░░░░░░░░░░]"
+	ldbarRune := []rune(ldbar)
+	for i := 1; i < len(ldbarRune)-1; i++ {
+		Clear()
+		ldbarRune[i] = rune('█')
+		PrintColor(string(ldbarRune), "Red")
+		time.Sleep(t * time.Millisecond) //pauses for t milliseconds
+	}
+	Clear()
+	PrintColor(string(ldbarRune), "Green")
+	time.Sleep(500 * time.Millisecond)
+	Clear()
+	PrintColor("Loading Complet\n", "Green")
+	time.Sleep(500 * time.Millisecond)
+
 }
