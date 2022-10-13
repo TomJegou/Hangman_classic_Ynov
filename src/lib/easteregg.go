0dbf92c5337e8b0e667a2223098c84764adda28c
L0uxe <loulourxl@hotmail.com>
2022-10-13 11:40:30 +0200

    add credits

--- src/lib/easteregg.go
+++ src/lib/easteregg.go
@@ -1,5 +1,6 @@
 package lib
 
+// art from http://www.ascii-art.de/ascii/s/stickman.txt and https://ascii.co.uk/art/hangman
 func EasterEgg(filename string) {
 	file := GetFileInLine("../Templates/.foreasteregg/" + filename + ".txt")
 	for i := 0; i < len(file); i++ {
