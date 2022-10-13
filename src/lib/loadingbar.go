18f3615c9b5061de446ab7cb498dd4f2679dea2b
L0uxe <loulourxl@hotmail.com>
2022-10-13 08:31:15 +0200

    add comment

--- src/lib/loadingbar.go
+++ src/lib/loadingbar.go
@@ -4,15 +4,18 @@ import (
 	"time"
 )
 
-func LoadingBar(t time.Duration) { //t = time in millisecond
+/*
+Func that make a loding bar
+*/
 
+func LoadingBar(t time.Duration) { //t = time in millisecond
 	ldbar := "[░░░░░░░░░░░░░░░░░░░░░░░░░░░]"
 	ldbarRune := []rune(ldbar)
 	for i := 1; i < len(ldbarRune)-1; i++ {
 		Clear()
 		ldbarRune[i] = rune('█')
 		PrintColor(string(ldbarRune), "Red")
-		time.Sleep(t * time.Millisecond)
+		time.Sleep(t * time.Millisecond) //pauses for t seconds
 	}
 	Clear()
 	PrintColor(string(ldbarRune), "Green")
