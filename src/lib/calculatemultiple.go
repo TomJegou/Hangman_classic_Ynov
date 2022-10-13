496be1d07098debeb96803aac19271e4ae25fbb8
TomJegou <tomjegou10@gmail.com>
2022-10-13 08:34:43 +0200

    add comments

--- src/lib/calculatemultiple.go
+++ src/lib/calculatemultiple.go
@@ -1,15 +1,22 @@
 package lib
 
+/*
+Function used to calculate the number of lines between each char
+in order to used different type of policies with different format
+We just count the number of line the first char of the file which
+is the char space.
+*/
+
 func CalculateMultiple(pathtofile string) int {
 	count := 1
 	t := GetFileInLine(pathtofile)
 	for i := 1; i < len(t); i++ {
-		count2 := 0
-		for j := 0; j < len(t[i]); j++ {
+		count2 := 0                      //count the number of spaces in a line, if the number is greater than 0 it's ok
+		for j := 0; j < len(t[i]); j++ { //looping into the line to chech every char in it
 			if string(t[i][j]) == " " {
 				count2++
 			}
-			if string(t[i][j]) != " " {
+			if string(t[i][j]) != " " { // if there is something else than a space, it means that we are in the other char
 				return count
 			}
 		}
